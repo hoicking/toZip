424a92a0b0790e746065ce7a5f312b74b8e9d65c
wangshengyang <wangshengyang@yxt.com>
2022-09-13 17:54:13 +0800

    aa

--- src/pages/right.js
+++ src/pages/right.js
@@ -1,12 +1,11 @@
 import React, {useEffect, useImperativeHandle, useState} from 'react'
 import html2canvas from 'html2canvas'
+import streamSaver from 'streamsaver'
+
 import JSZip from 'jszip'
 import { saveAs } from 'file-saver'
 
-import * as fflate from 'fflate'
-
-
-import { createDownloadStream } from './util'
+import {ZIP} from './zip'
 
 
 function Index ({cref}) {
@@ -15,6 +14,8 @@ function Index ({cref}) {
   }, [])
 
   const toImage = async () => {
+    const fileStream = streamSaver.createWriteStream('archive.zip')
+
     const data = await getData()
 
     const el = document.getElementById('left')
@@ -30,127 +31,93 @@ function Index ({cref}) {
 
     const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
 
-    const list = []
-    data.forEach(item => {
-      ctx.putImageData(imageData, 0, 0)
-      list.push(getBlob(item, ctx, canvas))
-    })
-    const res = await Promise.all(list)
-
     console.log('-----> read blob ok')
 
-    blobToStream(res)
 
-    // const zip = new JSZip()
-    // res.forEach((blob, index)=> {
-    //     // 添加要压缩的pdf
-    //     zip.file(`${index}.png`, blob, { binary:true })
-    // })
-    // zip.generateAsync({type:'blob'}).then(function(content) {
-    //     //生成zip并保存
-    //     saveAs(content, '文件.zip')
-    // })
-  }
+    const readableZipStream = new ZIP({
+      start (ctrl) {
 
-  const getBlob = async(textInfo, ctx, canvas) => {
-    // ctx.clearRect(0, 0, canvas.width, canvas.height)
-    textInfo.forEach(item => {
-      ctx.fillStyle = item.color
-      ctx.font = `${item.font}px serif`
-      ctx.fillText(item.text, item.x, item.y)
+        // const blob = new Blob(['support blobs too'])
+
+        // const file3 = {
+        //   name: 'streamsaver-zip-example/blob-example.txt',
+        //   stream: () => blob.stream()
+        // }
+        
+        // ctrl.enqueue(file3)
+        // ctrl.close()
+      },
+      async pull (ctrl) {
+        console.log('----->pull')
+        const getStream = async(textInfo, ctx, canvas, index) => {
+          // ctx.clearRect(0, 0, canvas.width, canvas.height)
+          textInfo.forEach(item => {
+            ctx.fillStyle = item.color
+            ctx.font = `${item.font}px serif`
+            ctx.fillText(item.text, item.x, item.y)
+          })
+
+          return new Promise((resolve, reject) => {
+            canvas.toBlob(function (blob) {
+              console.log(`------>${index}`, blob)
+              ctrl.enqueue({name:`blob-example${index}.png`, stream: () => blob.stream() })
+              resolve(blob.stream())
+              // resolve(blob)
+            })
+          })
+        }
+        // Gets executed everytime zip.js asks for more data
+        // const url = 'https://d8d913s460fub.cloudfront.net/videoserver/cat-test-video-320x240.mp4'
+        // const res = await fetch(url)
+        // const stream = () => res.body
+        // const name = 'streamsaver-zip-example/cat.mp4'
+        const list = []
+        // data.forEach((item, index) => {
+        //   ctx.putImageData(imageData, 0, 0)
+        //   list.push(getStream(item, ctx, canvas, index))
+        // })
+        // const res =  await Promise.all(list)
+        for (let index = 0; index < data.length; index++) {
+          await getStream(data[index], ctx, canvas, index)
+        }
+
+        // const zip = new JSZip()
+        // res.forEach((blob, index)=> {
+        //     // 添加要压缩的pdf
+        //     zip.file(`${index}.png`, blob, { binary:true })
+        // })
+        // zip.generateAsync({type:'blob'}).then(function(content) {
+        //     //生成zip并保存
+        //     saveAs(content, '文件.zip')
+        // })
+        // if (done adding all files)
+        ctrl.close()
+      }
     })
 
-    return new Promise((resolve, reject) => {
-      canvas.toBlob(function (blob) {
-        resolve(blob)
-      })
-    })
+    const writer = fileStream.getWriter()
+    const reader = readableZipStream.getReader()
+    const pump = () => reader.read()
+      .then(res => res.done ? writer.close() : writer.write(res.value).then(pump))
 
+    pump()
   }
 
-  const blobToStream = async (blobs) => {
+  // const getStream = async(textInfo, ctx, canvas) => {
+  //   // ctx.clearRect(0, 0, canvas.width, canvas.height)
+  //   textInfo.forEach(item => {
+  //     ctx.fillStyle = item.color
+  //     ctx.font = `${item.font}px serif`
+  //     ctx.fillText(item.text, item.x, item.y)
+  //   })
 
-    try {
-      const stream = (await createDownloadStream('file.zip')).getWriter()
+  //   return new Promise((resolve, reject) => {
+  //     canvas.toBlob(function (blob) {
+  //       resolve(blob.stream())
+  //     })
+  //   })
+  // }
 
-      const buffer = bufferMacroTaskChunk((chunk) => {
-        stream.write(chunk).then(() => {
-          iterator.next()
-        })
-      })
-      const zip = new fflate.Zip((err, data, final) => {
-        if (err || final) {
-          stream.close()
-          console.log("end")
-        } else {
-          console.log("pull", data)
-          buffer(data)
-        }
-      })
-
-      const streamList = []
-
-      for (let i = 0; i < blobs.length; i++) {
-        const blob = blobs[i]
-        const zipStream = new fflate.ZipDeflate(`王尼玛-${i}.png`, {
-          level: 6,
-        })
-        streamList.push([blob.stream().getReader(), zipStream])
-        zip.add(zipStream)
-      }
-
-      async function* read() {
-        for (let i = 0; i < streamList.length; i++) {
-          const [reader, zipStream] = streamList[i]
-          while (true) {
-            console.log("read")
-            const { done, value = new Uint8Array() } = await reader.read()
-            console.log("push", i, done)
-            zipStream.push(value, done)
-            console.log("yield")
-            yield
-            console.log("next")
-            if (done) break
-          }
-        }
-        zip.end()
-      }
-      const iterator = read()
-      console.log("start")
-      iterator.next()
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  const concatUnit8Array = (chunks) => {
-    const len = chunks.reduce((len, { length }) => len + length, 0)
-    const mergeArr = new Uint8Array(len)
-    chunks.reduce((offset, chunk) => {
-      mergeArr.set(chunk, offset)
-      return offset + chunk.length
-    }, 0)
-    return mergeArr
-  }
-
-  //
-  const bufferMacroTaskChunk = (callback) => {
-    const chunks = []
-    let flag = false
-    return (chunk) => {
-      chunks.push(chunk)
-      if (!flag) {
-        flag = true
-        setTimeout(() => {
-          console.log('----> buffer macrotask run')
-          flag = false
-          const mergeArr = concatUnit8Array(chunks)
-          chunks.length = 0
-          callback(mergeArr)
-        })
-      }
-    }
-  }
 
   useImperativeHandle(cref, () => ({
     toImage: () => {
@@ -160,7 +127,7 @@ function Index ({cref}) {
 
   const getData = async() => {
     let list = []
-    for (let index = 0; index < 10; index++) {
+    for (let index = 0; index < 1000; index++) {
       list.push([{text: `王尼玛${index}`, x: 120, y: 360, color: 'orange', fontsize: '24'},
       {text: '12341231', x: 190, y: 640, color: 'orange', fontsize: '24'},
       {text: '20220718', x: 390, y: 640, color: 'orange', fontsize: '24'}])
